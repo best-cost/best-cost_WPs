1658d9d43936ab3942de1829e94e9c7d899c3c9a
luytax <axel.luyten@gmail.com>
2024-02-15 12:44:46 +0100

    Checked fct get_paf()

--- r_package/bestcost/R/get_paf.R
+++ r_package/bestcost/R/get_paf.R
@@ -2,11 +2,9 @@
 
 #' Get population attributable fraction
 #'
-#' Calculates the population attributable fraction of an environmental stressor
-#' @param crf_conc
-#' Risk estimate of the concentration response function for a specific concentration. The population attributable fraction is normally calculated using the risk estimate that refers to the concentration that reflects the population exposure and the cut-off. This risk estimate is obtained after re-scaling from the epidemiological study with a particular increment (e.g. for PM2.5 10 or 5 ug/m3) to the aimed concentration.
-#' @param prop_pop_exp
-#' Proportion (from 0 to 1) of population exposed to the environmental stressor. Per default = 1 (i.e. 100\% of population is exposed).
+#' Calculates the population attributable fraction of a health outcome due to exposure to an environmental stressor
+#' @param crf_conc \code{Numerical value} Risk estimate of the concentration response function for a specific concentration. The population attributable fraction is normally calculated using the risk estimate that refers to the concentration that reflects the population exposure and the cut-off. This risk estimate is obtained after re-scaling from the epidemiological study with a particular increment (e.g. for PM2.5 10 or 5 ug/m3) to the aimed concentration.
+#' @param prop_pop_exp \code{Numerical value} Fraction ([0,1]) of population exposed to the environmental stressor. Per default = 1 (i.e. 100\% of population is exposed).
 #' @return
 #' This function returns a \code{value} corresponding to the population attributable fraction
 #' @examples
@@ -16,7 +14,10 @@
 #' @export
 get_paf <-
   function(crf_conc, prop_pop_exp){
-    # Source: https://pophealthmetrics.biomedcentral.com/articles/10.1186/1478-7954-1-1
+    # Sources:
+    # WHO 2003 a: Prüss-Üstün_2003_Assessing the environmental burden of disease at national and local levels)
+    # WHO 2003 b: Murray_2003_Comparative quantification of health risks Conceptual framework and methodological issues
+    # GBD 2019
     paf <- (sum(prop_pop_exp * (crf_conc-1))) / (1+(sum(prop_pop_exp *(crf_conc-1))))
 
     return(paf)
