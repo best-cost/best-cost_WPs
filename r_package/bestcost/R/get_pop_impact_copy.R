9ac869e3e1b737c16fffb3a8e7f85d8eba53a0d5
luytax <axel.luyten@gmail.com>
2024-06-13 11:07:53 +0200

    De-nested function "get_pop_impact" (1st draft)
    The de-nested function is named "get_pop_impact_copy" and integrated in the main function "attribute_copy". Both the YLL and premature deaths results stayed exactly the same.
    In a next step, the de-nested code will be inserted into "get_pop_impact".
    
    Both YLL and premature deaths assessments got around 10% faster each.

--- r_package/bestcost/R/get_pop_impact_copy.R
+++ r_package/bestcost/R/get_pop_impact_copy.R
@@ -28,48 +28,79 @@
 get_pop_impact_copy <-
   function(lifetab_withPop, year_of_analysis, pop_fraction, outcome_metric){
 
-
-    # Get popOvertime
-    popOverTime <- list()
-
-    for(s in names(lifetab_withPop)){
-      for(v in pop_fraction$erf_ci){
-        popOverTime[[s]][[paste0("erf_ci_",v)]] <-
-
-          # Add the first year of the life table to the column name of population
-          lifetab_withPop[[s]] <- lifetab_withPop[[s]] %>%
-            dplyr::rename(!!paste0("population_", year_of_analysis) := population)
-
-          bestcost::project_pop(
-            lifetab_withPop = lifetab_withPop[[s]],
-            year_of_analysis = year_of_analysis,
-            paf = pop_fraction$paf[pop_fraction$erf_ci %in% v],
-            outcome_metric = outcome_metric)
-      }
-    }
-
-
-
-
-    # Get pop_impact
-
+    lifetable_withPop <- lifetab_withPop # To be done: change function argument name to "lifetable_withPop
+    # popOverTime <- list()
     pop_impact <- list()
 
-    for(s in names(lifetab_withPop)){
-      for(v in pop_fraction$erf_ci){
-        pop_impact[[s]][[paste0("erf_ci_",v)]] <-
+    lifetable_withPop_backup <- lifetable_withPop
 
-          bestcost::move_rows_up(popOTime = popOverTime[[s]][[paste0("erf_ci_",v)]],
-                                 year_of_analysis = year_of_analysis)
+    for(s in names(lifetable_withPop)){
+      lifetable_withPop <- lifetable_withPop_backup
+      # print(s)
+      for(v in pop_fraction$erf_ci){
+        # print(v)
+
+        lifetable_withPop = lifetable_withPop_backup[[s]]
+        paf = pop_fraction$paf[pop_fraction$erf_ci %in% v]
+
+
+        #### project_pop code #################################################################################
+
+        # Add the first year of the life table to the column name of population
+        lifetable_withPop <- lifetable_withPop %>%
+          dplyr::rename(!!paste0("population_", year_of_analysis) := population)
+
+        ##### project_pop_withExp ##############################################################################
+        second_year <- year_of_analysis + 1
+
+        lifetable_withPop <-
+          lifetable_withPop %>%
+          # Calculate the population the second year (first column after first year) considering the health effect of air pollution
+          dplyr::mutate("population_{second_year}" := dplyr::lag(!!as.symbol(paste0("population_",
+                                                                                    year_of_analysis))) * dplyr::lag(death_probability_natural) * paf) %>%
+          # Move column up one row
+          dplyr::mutate("population_{second_year}" := lead(!!as.symbol(paste0("population_",
+                                                                              second_year))))
+
+        #### project_pop code ##############################################################################
+
+        if(outcome_metric %in% c("yll", "yld")){
+          # Now calculate population over time (for the rest of years)
+          # without considering air pollution
+
+          lifetable_withPop = lifetable_withPop
+          year_of_analysis = year_of_analysis
+
+          ##### project_pop_noExp #######################################################################
+          lifetable_withPop <-
+            lifetable_withPop
+
+          years <- c( (year_of_analysis + 1) : ((year_of_analysis + nrow(lifetable_withPop) - 2)) )
+          length_period <- length(years)
+
+          for (i in 0:(length_period-1)){ # i in 0:97
+            YEAR <- years[i+1]
+            # print(YEAR+1)
+            # print(i)
+            lifetable_withPop[1:((length_period)-i), paste0("population_", YEAR+1)] <-
+              lifetable_withPop[1:((length_period)-i), paste0("population_", YEAR)] * (1 - lifetable_withPop$death_probability_total[(i+2):(length_period+1)])
+          }
+
+
+        }
+
+        #### project_pop ###################################################################################
+
+        ### get_pop_impact #################################################################################
+        # popOverTime[[s]][[paste0("erf_ci_",v)]] <- lifetable_withPop
+        # pop_impact[[s]][[paste0("erf_ci_",v)]] <- popOverTime[[s]][[paste0("erf_ci_",v)]]# New code replacing the call of move_rows_up
+        pop_impact[[s]][[paste0("erf_ci_",v)]] <- lifetable_withPop
       }
     }
-
-
     output <-
       list(paf = pop_fraction,
-           popOverTime = popOverTime,
+           # popOverTime = popOverTime,
            pop_impact = pop_impact)
-
 
     return(output)
 
