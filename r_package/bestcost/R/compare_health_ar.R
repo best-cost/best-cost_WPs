b8322712cae23a88f822b83396a2f35bda129e7e
ungatoverde <alberto.acf@gmail.com>
2024-06-13 11:32:06 +0200

    compare_health_ar() and compare_yld_from_prevalence() now as wrapper functions of compare()

--- r_package/bestcost/R/compare_health_ar.R
+++ r_package/bestcost/R/compare_health_ar.R
@@ -29,57 +29,45 @@
 #' @note Experimental function
 #' @export
 compare_health_ar <-
-  function(exp_central_1, exp_lower_1 = NULL, exp_upper_1 = NULL,
+  function(comparison_method = "delta",
+           exp_central_1, exp_lower_1 = NULL, exp_upper_1 = NULL,
            exp_central_2, exp_lower_2 = NULL, exp_upper_2 = NULL,
            pop_exp_1, pop_exp_2,
            erf_c_central, erf_c_lower = NULL, erf_c_upper = NULL,
            info_1 = NULL, info_2 = NULL){
 
-
-
-    # Calculate attributable health impacts in the scenario 1
-    att_health_1 <-
-      bestcost::attribute_health_singlebhd_ar(
-        exp_central = exp_central_1,
-        pop_exp = pop_exp_1,
-        erf_c_central = erf_c_central,
-        info = info_1)
-
-    # Calculate attributable health impacts in the scenario 2
-    att_health_2 <-
-      bestcost::attribute_health_singlebhd_ar(
-        exp_central = exp_central_2,
-        pop_exp = pop_exp_2,
-        erf_c_central = erf_c_central,
-        info = info_2)
-
-    # Identify the columns that are common for scenario 1 and 2
-    # grepl instead of %in% because there might be differnt info columns starting with info_
-    joining_columns <-
-      names(att_health_1[["main"]])[! grepl(c("exp|rr_conc|paf|absolute_risk_as_percent|population_affected|impact|impact_rounded|info"),
-                                             names(att_health_1[["main"]]))]
-
-    # Merge the result tables by common columns
-    att_health <-
-      dplyr::left_join(
-        att_health_1[["main"]],
-        att_health_2[["main"]],
-        by = joining_columns,
-        suffix = c("_1", "_2"))%>%
-      # Calculate the delta (difference) between scenario 1 and 2
-      dplyr::mutate(impact = impact_1 - impact_2)
-
-    # No PIF option because the absolute risk does not have population fraction
-    # So only delta option
-
-      # Round results
-      att_health <-
-        att_health %>%
-        mutate(impact_rounded = round(impact, 0))
-
-   output <- list(main = att_health,
-                  detailed = list(scenario_1 = att_health_1,
-                                  scenario_2 = att_health_2))
+    output <-
+      bestcost::compare(
+        comparison_method = comparison_method,
+        health_metric = "same_input_output",
+        risk_method = "absolute_risk",
+        exp_central_1, exp_lower_1 = exp_lower_1, exp_upper_1 = exp_upper_1,
+        exp_central_2, exp_lower_2 = exp_lower_2, exp_upper_2 = exp_upper_2,
+        prop_pop_exp_1 = NULL,
+        prop_pop_exp_2 = NULL,
+        pop_exp_1 = pop_exp_1,
+        pop_exp_2 = pop_exp_2,
+        cutoff = NULL,
+        rr_central = NULL , rr_lower = NULL, rr_upper = NULL,
+        erf_increment = NULL,
+        erf_shape = NULL,
+        erf_c_central = erf_c_central, erf_c_lower = erf_c_lower, erf_c_upper = erf_c_upper,
+        bhd_central_1 = NULL, bhd_lower_1 = NULL, bhd_upper_1 = NULL,
+        bhd_central_2 = NULL, bhd_lower_2 = NULL, bhd_upper_2 = NULL,
+        disability_weight = NULL,
+        duration = NULL,
+        first_age_pop_1 = NULL, last_age_pop_1 = NULL,
+        prob_natural_death_male_1 = NULL, prob_natural_death_female_1 = NULL,
+        prob_total_death_male_1 = NULL, prob_total_death_female_1 = NULL,
+        population_midyear_male_1 = NULL, population_midyear_female_1 = NULL,
+        year_of_analysis_1 = NULL,
+        first_age_pop_2 = NULL, last_age_pop_2 = NULL,
+        prob_natural_death_male_2 = NULL, prob_natural_death_female_2 = NULL,
+        prob_total_death_male_2 = NULL, prob_total_death_female_2 = NULL,
+        population_midyear_male_2 = NULL, population_midyear_female_2 = NULL,
+        year_of_analysis_2 = NULL,
+        min_age = NULL, max_age = NULL,
+        info_1 = info_1, info_2 = info_2)
 
 
     return(output)
