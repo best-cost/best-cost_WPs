b0813fc89c8bacad05dd951e840bc72770c26ed6
ungatoverde <alberto.acf@gmail.com>
2024-06-24 16:44:52 +0200

    pop_fraction instead of pif or paf functions working in attribute funcions until deaths from lifetable and exp distribution

---
+++ r_package/bestcost/R/get_risk_and_pop_fraction.R
@@ -0,0 +1,67 @@
+# Title and description
+
+#' Get input data and PAF
+
+#' Calculates the population attributable fraction (PAF) based on the input data and puts the results in additional columns joined to the input data frame.
+#' @param input \code{Data frame} with the input data
+#' @return
+#' This function returns a \code{data.frame} with the input data adding a column for the population attributable fraction
+#' Moreover, the data frame includes columns such as:
+#' \itemize{
+#'  \item Attributable fraction
+#'  \item Health impact
+#'  \item Outcome metric
+#'  \item And many more.
+#' }
+#' @import dplyr
+#' @import purrr
+#' @examples
+#' TBD
+#' @author Alberto Castro
+#' @note Experimental function
+#' @keywords internal
+
+get_risk_and_pop_fraction <-
+  function(input){
+
+    # Calculate health impact attributable to exposure ####
+    input_with_risk_and_pop_fraction <-
+      input %>%
+      dplyr::rowwise(.) %>%
+      dplyr::mutate(
+        # Obtain the relative risk for the relevant concentration
+        rr_conc =
+          bestcost::get_risk(rr = rr,
+                             exp = exp,
+                             cutoff = cutoff,
+                             erf_increment = erf_increment,
+                             erf_shape = erf_shape))%>%
+      # Calculate population (attributable or impact) fraction (PAF or PIF) ####
+      # Group by exp in case that there are different exposure categories
+      dplyr::group_by(erf_ci, exp_ci) %>%
+      dplyr::mutate(
+        pop_fraction = bestcost::get_pop_fraction(rr_conc_1 = rr_conc,
+                                                  rr_conc_2 = 1,
+                                                  prop_pop_exp_1 = prop_pop_exp,
+                                                  prop_pop_exp_2 = 1))%>%
+      dplyr::ungroup(.)
+
+    # Data wrangling ####
+    # Only if exposure distribution (multiple exposure categories)
+    # then reduce the number of rows to keep the same number as in rr
+    # if(unique(input$exposure_type) == "exposure_distribution"){
+    #   input_risk_paf <-
+    #     input_risk_paf %>%
+    #     dplyr::mutate(
+    #       # Add a column for the average exp (way to summarize exposure)
+    #       exp_mean = mean(exp),
+    #       # Replace the actual values with "multiple" to enable reduction of rows
+    #       exp = paste(exp, collapse = ", "),
+    #       prop_pop_exp = paste(prop_pop_exp, collapse = ", "),
+    #       rr_conc = paste(rr_conc, collapse = ", ")) %>%
+    #     # Keep only rows that are distinct
+    #     dplyr::distinct(.)
+    # }
+
+    return(input_with_risk_and_pop_fraction)
+  }
