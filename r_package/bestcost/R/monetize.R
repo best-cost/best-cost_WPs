41c26237296121a29713e8c9be9a183109e21f49
ungatoverde <alberto.acf@gmail.com>
2024-09-16 13:32:30 +0200

    Changes in monetize to include valuation as argument

--- r_package/bestcost/R/monetize.R
+++ r_package/bestcost/R/monetize.R
@@ -2,14 +2,16 @@
 #'
 #' Monetize health impacts (internal function)
 #'
-#' @param impact_raw
+#' @param output \code{List} produced by \code{bestcost::attribute()} or \code{bestcost::compare()} as results
+#' @param valuation \code{Numeric value} showing the value of statistical life which will be used in the health impact monetization
 #'
 #' @return Description of the return value.
 #' @examples
 #' # Example of how to use the function
 #' function_name(param1 = value1, param2 = value2)
 #' @export
-monetize <- function(output) {
+monetize <- function(output,
+                     valuation) {
 
   output[["main"]] <- output[["main"]] %>%
     dplyr::mutate(impact_monetized = as.numeric(valuation) * as.numeric(impact),
