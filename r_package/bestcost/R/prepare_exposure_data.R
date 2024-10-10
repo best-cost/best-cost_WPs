ca796e474603e643a6eccea8b8d9aa583d5245fd
Axel Luyten <axel.luyten@gmail.com>
2024-10-10 14:57:28 +0200

    Create function skeleton + documentation
    Note: this text doesn't show up in the commit title

---
+++ r_package/bestcost/R/prepare_exposure_data.R
@@ -0,0 +1,25 @@
+#' Prepare exposure data
+
+#' @description
+#' This function ...
+#' @param erf_ci \code{Vector} with the central, lower and upper estimate of the relative risk
+#' @return
+#' This function returns ...
+#' @import dplyr
+#' @import purrr
+#' @export
+#' @author Arno Pauwels
+#' @note Experimental function
+#' @keywords internal
+#' @examples
+#' TBD
+
+prepare_exposure_data <-
+  function(variable_name){
+
+
+
+
+
+
+  }
