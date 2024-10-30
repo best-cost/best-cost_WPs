f421a4c568285452b41bd6a961aaf220e2ea4e46
Axel Luyten <axel.luyten@gmail.com>
2024-10-30 16:42:46 +0100

    Resolve conflicts

---
+++ r_package/healthiar/R/include_social.R
@@ -0,0 +1,47 @@
+#' include_social
+
+#' @description Consider socio-economic aspects in the results
+#' @param output \code{List} produced by \code{healthiar::attribute()} or \code{healthiar::compare()} as results
+#' @param deprivation_weighting \code{Vector} with numeric values showing the deprivation index (indicator of economic wealth) of the fine geographical area (it should match with those used in \code{attribute} or \code{compare})
+#' @inheritParams attribute
+#'
+#' @return Description of the return value.
+#' @examples
+#' # Example of how to use the function
+#' function_name(param1 = value1, param2 = value2)
+#' @export
+include_social <- function(output,
+                           deprivation_weight,
+                           geo_id_raw
+                           #TODO Add method (if multiple)
+                           ) {
+
+  output_social <- list()
+
+  # Re-calculate output with the social aspects inside using output_raw
+
+  output_social[["main"]] <-
+    output[["detailed"]][["raw"]] |>
+    dplyr::left_join(
+      .,
+      dplyr::tibble(geo_id_raw = geo_id_raw,
+                    deprivation_weight = deprivation_weight),
+      by = "geo_id_raw")|>
+    dplyr::mutate(impact_deprivation_weighted =
+                    as.numeric(impact) * as.numeric(deprivation_weight),
+                  .after = impact) |>
+    dplyr::mutate(impact_rounded_deprivation_weighted =
+                    round(impact_deprivation_weighted),
+                  .after = impact_rounded)
+
+
+  # Based on the new output_raw that includes social aspects
+  # Recalculate output
+  output_social <- healthiar:::get_output(output_social)
+
+  # Calculate the weighting
+
+
+  return(output_social)
+
+}
---
+++ r_package/healthiar/R/prepare_exposure_data.R
@@ -0,0 +1,26 @@
+#' Prepare exposure data
+
+#' @description
+#' This function prepares tabular population exposure data compatible with the attribute() and compare() functions, based on spatial pollution and geographic units data.
+#' @param poll_grid \code(SpatRaster) of the pollution data.
+#' @param geo_units \code(sf) of the geographic units data.
+#' @return
+#' This function returns a vector of population exposure values. 
+#' @import terra
+#' @import sf
+#' @export
+#' @author Arno Pauwels
+#' @note Experimental function
+#' @keywords internal
+#' @examples
+#' TBD
+
+prepare_exposure_data <-
+  function(
+    poll_grid,
+    geo_units
+  ){## calculate mean concentration in each geo unit
+    exp <- terra::extract(poll_grid, geo_units, fun = mean)
+    return(as.vector(exp[,2]))
+  }
+
