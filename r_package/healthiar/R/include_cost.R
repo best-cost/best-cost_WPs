6714b66ab45e4c92f7da1877765edd943f6c40c1
Alberto Castro Fernández <castal@swisstph.ch>
2024-12-05 14:34:36 +0100

    New functions (code) in include_cost to accomodate new variables: Not working:-(to be fixed)

--- r_package/healthiar/R/include_cost.R
+++ r_package/healthiar/R/include_cost.R
@@ -3,6 +3,7 @@
 #' @description Monetize health impacts
 #'
 #' @param output \code{List} produced by \code{healthiar::attribute()} or \code{healthiar::compare()} as results
+#' @param time_period \code{Numeric value} referring to the period of time to be considered in the discounting.
 #' @param valuation \code{Numeric value} showing the value of statistical life which will be used in the health impact monetization
 #'
 #' @return Description of the return value.
@@ -13,15 +14,39 @@
 include_cost <- function(output,
                          valuation,
                          corrected_discount_rate = NULL,
-                         time_period = NULL) {
+                         time_period = NULL,
+                         approach_discount = NULL) {
 
   # Define a function to add the monetized impacts (rounded and not rounded)
   add_monetized_impact <-
     function(df){
-      df |>
-        dplyr::mutate(valuation = valuation,
-                      impact_monetized = as.numeric(valuation) * as.numeric(impact),
-                      .after = impact) |>
+      df_with_cost <-
+        df |>
+        # Add columns for input data in the table
+        dplyr::mutate(corrected_discount_rate = {{corrected_discount_rate}},
+                      time_period = {{time_period}},
+                      approach_discount = {{approach_discount}}) |>
+        # Calculate discount factor
+        # If any of the columns "time_period" and "approach_discount" are not present
+        # it is because their argument are NULL
+        # So no discount (i.e. discount_factor=1)
+        #dplyr::rowwise() |>
+        dplyr::mutate(
+          discount_factor =
+            ifelse(
+              any(is.null({{corrected_discount_rate}}),
+                  is.null({{time_period}}),
+                  is.null({{approach_discount}})),
+              healthiar::get_discount_factor(
+                corrected_discount_rate = corrected_discount_rate,
+                time_period = time_period,
+                approach_discount = approach_discount),
+              1),
+          # Add column for valuation
+          valuation = valuation,
+          # Calculate monetized impact
+          impact_monetized = impact * discount_factor * valuation,
+          .after = impact) |>
         dplyr::mutate(impact_rounded_monetized = round(impact_monetized),
                       .after = impact_rounded)
     }
